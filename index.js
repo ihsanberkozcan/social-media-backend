8e461876ec388ac072c47eb254db7ecf38b48b0b
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-06-07 22:26:12 +0300

    User

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import "dotenv/config";
+
+import usersRouter from './routes/users.js';
+import authRouter from './routes/auth.js';
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected!"));
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.listen(8800, () => {
+  console.log("Connected");
+});
+
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
